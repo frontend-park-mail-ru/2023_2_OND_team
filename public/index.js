e48fc591c3d9cadf5fbaf18309cfbe3f1c3c2b4d
Muslim Mamadaev <mamad.muslim@mail.ru>
2023-10-01 00:53:51 +0300

    add creation, sending and processing of cookies during aut

--- public/index.js
+++ public/index.js
@@ -1,6 +1,6 @@
 const fullscreenImage = document.getElementById('fond');
 
-renderRegPage()
+renderAuthPage()
 
 function createLabeledInput(labelText, inputType, inputPlaceholder) {
     const label = document.createElement('label');
@@ -33,13 +33,16 @@ function renderAuthPage() {
     form.classList.add('input-container');
     whiteBlock.appendChild(form);
 
-    form.appendChild(createLabeledInput('Имя пользователя', 'text', 'username'));
-    form.appendChild(createLabeledInput('Пароль', 'password', '●●●●●●●●●●●●'));
+    const usernameInput = createLabeledInput('Имя пользователя', 'text', 'username');
+    const passwordInput = createLabeledInput('Пароль', 'password', '●●●●●●●●●●●●');
 
-    const submitButton = document.createElement('button');
-    submitButton.type = 'submit';
-    submitButton.textContent = 'Войти';
-    form.appendChild(submitButton);
+    form.appendChild(usernameInput);
+    form.appendChild(passwordInput);
+
+    const AuthButton = document.createElement('button');
+    AuthButton.type = 'submit';
+    AuthButton.textContent = 'Войти';
+    form.appendChild(AuthButton);
 
     const textContainer = document.createElement('p');
 
@@ -58,6 +61,15 @@ function renderAuthPage() {
         e.preventDefault();
         renderRegPage();
     });
+
+    AuthButton.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const username = usernameInput.querySelector('input').value;
+        const password = passwordInput.querySelector('input').value;
+
+        loginUser(username, password);
+    });
 }
 
 function renderRegPage() {
@@ -75,14 +87,18 @@ function renderRegPage() {
     form.classList.add('input-container');
     whiteBlock.appendChild(form);
 
-    form.appendChild(createLabeledInput('Имя пользователя', 'text', 'username'));
-    form.appendChild(createLabeledInput('Почта', 'email', 'test@mail.ru'));
-    form.appendChild(createLabeledInput('Пароль', 'password', '●●●●●●●●●●●●'));
+    const usernameInput = createLabeledInput('Имя пользователя', 'text', 'username');
+    const emailInput = createLabeledInput('Почта', 'email', 'test@mail.ru');
+    const passwordInput = createLabeledInput('Пароль', 'password', '●●●●●●●●●●●●');
 
-    const submitButton = document.createElement('button');
-    submitButton.type = 'submit';
-    submitButton.textContent = 'Создать аккаунт';
-    form.appendChild(submitButton);
+    form.appendChild(usernameInput);
+    form.appendChild(emailInput);
+    form.appendChild(passwordInput);
+
+    const RegButton = document.createElement('button');
+    RegButton.type = 'submit';
+    RegButton.textContent = 'Создать аккаунт';
+    form.appendChild(RegButton);
 
     const textContainer = document.createElement('p');
 
@@ -100,5 +116,71 @@ function renderRegPage() {
     signInLink.addEventListener('click', function (e) {
         e.preventDefault();
         renderAuthPage();
+    });
+}
+
+function setCookie(name, value, days) {
+    const date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + date.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+
+function getCookie(name) {
+    const cookieName = name + "=";
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const cookieArray = decodedCookie.split(';');
+    for (let i = 0; i < cookieArray.length; i++) {
+        let cookie = cookieArray[i];
+        while (cookie.charAt(0) === ' ') {
+            cookie = cookie.substring(1);
+        }
+        if (cookie.indexOf(cookieName) === 0) {
+            return cookie.substring(cookieName.length, cookie.length);
+        }
+    }
+    return "";
+}
+
+function sendCookieToServer(loggedInCookie) {
+    fetch('/api/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ loggedInCookie }),
+    })
+        .then((response) => {
+            if (response.ok) {
+                console.log('Cookie успешно отправлен на сервер');
+            } else {
+                console.error('Ошибка при отправке cookie на сервер');
+            }
+        })
+        .catch((error) => {
+            console.error('Ошибка при выполнении запроса:', error);
+        });
+}
+
+function loginUser(username, password) {
+    fetch('/api/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        if (data.success) {
+            setCookie('loggedIn', 'true', 7);
+            const loggedInCookie = getCookie('loggedIn');
+            sendCookieToServer(loggedInCookie);
+        } else {
+            console.error('Ошибка входа:', data.message);
+        }
+    })
+    .catch((error) => {
+        console.error('Ошибка при выполнении запроса:', error);
     });
 }
--- server/index.js
+++ server/index.js
@@ -2,18 +2,33 @@
 
 const express = require('express');
 const path = require('path');
+const cookieParser = require('cookie-parser');
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, '..', 'public')));
+app.use(express.json());
+app.use(cookieParser());
 
 app.get('/some-route', (require, response) => {
     const filePath = path.resolve(__dirname, '..', 'public', 'index.html');
-    
     response.sendFile(filePath);
-  });
+});
+
+app.post('/api/login', (require, response) => {
+    const { username, password } = require.body;
+
+    console.log('Имя пользователя:', username);
+    console.log('Пароль:', password);
+
+    if (username === 'test' && password === 'test') {
+        response.status(200).json({ success: true, message: 'Найс' });
+    } else {
+        response.status(401).json({ success: false, message: 'Неверное имя пользователя или пароль' });
+    }
+});
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server listening port ${PORT}`);
-})
+    console.log(`Server listening on port ${PORT}`);
+});
