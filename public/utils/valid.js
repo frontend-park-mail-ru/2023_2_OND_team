f71c8a77e819926fa693ee7c8474261330f56291
Muslim Mamadaev <mamad.muslim@mail.ru>
2023-10-04 01:07:20 +0300

    update validation

--- public/utils/valid.js
+++ public/utils/valid.js
@@ -7,8 +7,10 @@ export function isValidUserName(username) {
     const regex = /^[a-zA-Z0-9]+$/;
     let validation = { valid: true, message: '' };
 
-    if (username.length < 8) {
-        validation = { valid: false, message: 'Имя пользователя должно содержать не менее 8 символов.' };
+    if (username.length <= 4) {
+        validation = { valid: false, message: 'Имя пользователя должно содержать не менее 4 символов.' };
+    } else if (username.length >= 50) {
+        validation = { valid: false, message: 'Имя пользователя должно содержать менее 50 символов.' };
     } else if (!regex.test(username)) {
         validation = { valid: false, message: 'Имя пользователя должно состоять только из букв латинского алфавита и цифр.' };
     }
@@ -24,10 +26,20 @@ export function isValidUserName(username) {
 export function isValidEmail(email) {
     let validation = { valid: true, message: '' };
 
-    if (email.length < 4) {
-        validation = { valid: false, message: 'Email должен содержать не менее 4 символов.' };
+    if (email.length >= 50) {
+        validation = { valid: false, message: 'Email должен содержать не более 50 символов.' };
     } else if (!email.includes('@')) {
         validation = { valid: false, message: 'Email должен содержать символ @.' };
+    } else {
+        const parts = email.split('@');
+        if (parts.length !== 2) {
+            validation = { valid: false, message: 'Email должен содержать только один символ @.' };
+        } else {
+            const domainParts = parts[1].split('.');
+            if (domainParts.length < 2) {
+                validation = { valid: false, message: 'Некорректный формат домена в email.' };
+            }
+        }
     }
 
     return validation;
@@ -41,8 +53,10 @@ export function isValidEmail(email) {
 export function isValidPassword(password) {
     let validation = { valid: true, message: '' };
 
-    if (password.length < 8) {
+    if (password.length <= 8) {
         validation = { valid: false, message: 'Пароль должен содержать не менее 8 символов' };
+    } else if (password.length >= 50) {
+        validation = { valid: false, message: 'Пароль должен содержать менее 50 символов' };
     } else if (!/[a-z]/.test(password)) {
         validation = { valid: false, message: 'Пароль должен содержать хотя бы одну букву в нижнем регистре' };
     } else if (!/[A-Z]/.test(password)) {
