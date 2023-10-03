c08370e7dfa9ba4708267ebaa366cc20fc09099c
Romanlock <92111647+Romanlock@users.noreply.github.com>
2023-10-03 12:30:32 +0300

    added server starting

--- server/index.js
+++ server/index.js
@@ -8,12 +8,12 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const app = express();
 
-//const httpsOptions = {
-//    key: fs.readFileSync("/home/ond_team/cert/privkey.pem"), // путь к ключу
-//    cert: fs.readFileSync("/home/ond_team/cert/fullchain.pem") // путь к сертификату
-//}
+const httpsOptions = {
+   key: fs.readFileSync("/home/ond_team/cert/privkey.pem"), // путь к ключу
+   cert: fs.readFileSync("/home/ond_team/cert/fullchain.pem") // путь к сертификату
+}
 
-//https.createServer(httpsOptions, app).listen(1443);
+https.createServer(httpsOptions, app).listen(1443);
 
 app.use(express.static(path.resolve(__dirname, '..', 'public')));
 app.use(express.json());
