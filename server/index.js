6feceb94fdbcfac3f32841afca464cd16ad49802
Romanlock <92111647+Romanlock@users.noreply.github.com>
2023-10-04 19:09:00 +0300

    change server port

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ const httpsOptions = {
   cert: fs.readFileSync("/home/ond_team/cert/fullchain.pem") // путь к сертификату
 }
 
-https.createServer(httpsOptions, app).listen(1443);
+https.createServer(httpsOptions, app).listen(443);
 
 app.use(express.static(path.resolve(__dirname, '..', 'public')));
 app.use(express.json());
