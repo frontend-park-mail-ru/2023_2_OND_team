6650b28bec034d0da30f51476379fc3980fbb96b
Muslim Mamadaev <mamad.muslim@mail.ru>
2023-10-05 19:24:09 +0300

    add handlebars

--- public/components/Authorization/auth-page.js
+++ public/components/Authorization/auth-page.js
@@ -1,140 +1,29 @@
 import { renderRegPage } from '../Registration/reg-page.js';
-import { createLabeledInput } from '../Input/input.js';
-import { renderFeedPage } from '../Feed/Feed.js';
-import { emailValid, passwordValid, nameValid } from '../../utils/valid.js';
 import { API } from '../../utils/api.js';
+import { nameValid, passwordValid } from '../../utils/valid.js';
+
+const template = require('./auth-page.hbs'); // Загрузка шаблона
 
 /**
-* Рендерится страница аутентификации.
-* @param {HTMLElement} headerElement - Элемент заголовка.
-* @param {HTMLElement} pageElement - Элемент страницы.
-*/
+ * Рендерится страница аутентификации.
+ * @param {HTMLElement} headerElement - Элемент заголовка.
+ * @param {HTMLElement} pageElement - Элемент страницы.
+ */
 export function renderAuthPage(headerElement, pageElement) {
+    document.body.style.overflow = 'hidden';
+    const authPage = Handlebars.templates['Auth.hbs'];
     const Api = new API();
 
-    pageElement.innerHTML = '';
-    // pageElement.style.display = 'block';
-    document.body.style.overflow = 'hidden';
+    const context = {
+        
+    };
 
-    const cancelButton = document.createElement('button');
-    cancelButton.classList.add('cancel-button');
-    const cancelIcon = document.createElement('img');
-    cancelIcon.src = './static/svg/cancel.svg';
-    cancelButton.appendChild(cancelIcon);
-    pageElement.appendChild(cancelButton);
+    pageElement.innerHTML = authPage(context);
 
-    const ImageElement = document.createElement('img');
-    ImageElement.classList.add('ibackground');
-    ImageElement.src = 'static/img/image_2.png';
-    pageElement.appendChild(ImageElement);
-
-    const whiteBlock = document.createElement('div');
-    whiteBlock.classList.add('form-container');
-    pageElement.appendChild(whiteBlock);
-
-    const form = document.createElement('form');
-    form.classList.add('input-container');
-    whiteBlock.appendChild(form);
-
-    const usernameInput = createLabeledInput('Имя пользователя', 'text', 'username');
-    const passwordInput = createLabeledInput('Пароль', 'password', '●●●●●●●●●●●●');
-
-    const registrationTitle = document.createElement('h4');
-    registrationTitle.textContent = 'Войти в PinSpire';
-    registrationTitle.classList.add('auth-title');
-    form.appendChild(registrationTitle);
-
-    form.appendChild(usernameInput);
-    form.appendChild(passwordInput);
-
-    const AuthButton = document.createElement('button');
-    AuthButton.classList.add('button');
-    AuthButton.type = 'submit';
-    AuthButton.textContent = 'Войти';
-    form.appendChild(AuthButton);
-
-    const textContainer = document.createElement('p');
-    textContainer.classList.add('already-registered');
-
-    const alreadyHaveAccountText = document.createTextNode('Все еще нет аккаунта?');
-
-    const signUpLink = document.createElement('a');
-    signUpLink.href = '#';
-    signUpLink.textContent = 'Регистрация';
-
-    textContainer.appendChild(alreadyHaveAccountText);
-    textContainer.appendChild(signUpLink);
-
-    form.appendChild(textContainer);
-
-    signUpLink.addEventListener('click', function (e) {
-        e.preventDefault();
-        renderRegPage(headerElement, pageElement);
-    });
-
-    const passwordErrorSpan = document.createElement('span');
-    const usernameErrorSpan = document.createElement('span');
-
-    passwordErrorSpan.classList.add('error-message');
-    usernameErrorSpan.classList.add('error-message');
-
-    usernameInput.appendChild(usernameErrorSpan);
-    passwordInput.appendChild(passwordErrorSpan);
-
-    let errorSpan = document.querySelector('.error-mesage');
-
-    AuthButton.addEventListener('click', function (e) {
-        e.preventDefault();
-    
-        const username = usernameInput.querySelector('input').value;
-        const password = passwordInput.querySelector('input').value;
-
-        usernameInput.querySelector('input').style.borderColor = '';
-        passwordInput.querySelector('input').style.borderColor = '';
-    
-        const usernameValidationResult = nameValid(username);
-        const passwordValidationResult = passwordValid(password);
-    
-        if (!usernameValidationResult.valid) {
-            usernameInput.querySelector('input').style.borderColor = 'var(--error-50, #F4210B)';
-            usernameInput.querySelector('input').style.Color = 'var(--error-50, #F4210B)';
-            usernameErrorSpan.textContent = usernameValidationResult.message;
-        } else {
-            usernameErrorSpan.textContent = '';
-        }
-    
-        if (!passwordValidationResult.valid) {
-            passwordInput.querySelector('input').style.borderColor = 'var(--error-50, #F4210B)';
-            passwordInput.querySelector('input').style.Color = 'var(--error-50, #F4210B)';
-            passwordErrorSpan.textContent = passwordValidationResult.message;
-        } else {
-            passwordErrorSpan.textContent = '';
-        }
-    
-        if (usernameValidationResult.valid && passwordValidationResult.valid) {
-            
-            Api.loginUser(username, password)
-                .then(status => { 
-                    if (status) {
-                        headerElement.style.display = '';
-                        pageElement.style.paddingTop = '90px';
-                        renderFeedPage(headerElement, pageElement);
-                    } else {
-                        usernameInput.querySelector('input').style.borderColor = 'var(--error-50, #F4210B)';
-                        passwordInput.querySelector('input').style.borderColor = 'var(--error-50, #F4210B)';
-
-                        if (!errorSpan) {
-                            errorSpan = document.createElement('span');
-                            errorSpan.classList.add('error-message');
-                            errorSpan.textContent = 'Неверное имя пользователя или пароль';
-                            form.appendChild(errorSpan);
-                        }
-                        
-                    }
-                });
-            
-        }
-    });
+    const cancelButton = pageElement.querySelector('.cancel-button');
+    const form = pageElement.querySelector('form');
+    const usernameInput = form.querySelector('#username');
+    const passwordInput = form.querySelector('#password');
 
     cancelButton.addEventListener('click', function (e) {
         e.preventDefault();
@@ -142,19 +31,59 @@ export function renderAuthPage(headerElement, pageElement) {
         pageElement.style.paddingTop = '90px';
         renderFeedPage(headerElement, pageElement);
     });
-}
 
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
 
-// export function renderAuthPage(headerElement, pageElement) {
+        const username = usernameInput.value;
+        const password = passwordInput.value;
 
-//     document.body.style.overflow = 'hidden';
+        usernameInput.style.borderColor = '';
+        passwordInput.style.borderColor = '';
 
-//     const authPage = Handlebars.templates['Auth.hbs'];
+        const usernameValidationResult = nameValid(username);
+        const passwordValidationResult = passwordValid(password);
+
+        if (!usernameValidationResult.valid) {
+        handleValidationError(usernameInput, usernameValidationResult.message);
+        }
+
+        if (!passwordValidationResult.valid) {
+        handleValidationError(passwordInput, passwordValidationResult.message);
+        }
+
+        if (usernameValidationResult.valid && passwordValidationResult.valid) {
+        Api.loginUser(username, password)
+            .then(status => { 
+            if (status) {
+                headerElement.style.display = '';
+                pageElement.style.paddingTop = '90px';
+                renderFeedPage(headerElement, pageElement);
+            } else {
+                handleValidationError(form, 'Неверное имя пользователя или пароль');
+            }
+            });
+        }
+    });
+
+    const signUpLink = pageElement.querySelector('.already-registered a');
+    signUpLink.addEventListener('click', function (e) {
+        e.preventDefault();
+        renderRegPage(headerElement, pageElement);
+    });
+
+    function handleValidationError(element, message) {
+        const errorSpan = document.createElement('span');
+        errorSpan.classList.add('error-message');
+        errorSpan.textContent = message;
     
-//     const context = {
-       
-//     };
-
-//     pageElement.innerHTML = authPage(context);
-
-// }
+        const existingErrorSpan = element.querySelector('.error-message');
+        if (existingErrorSpan) {
+        existingErrorSpan.remove();
+        }
+    
+        element.appendChild(errorSpan);
+    
+        element.querySelector('input').style.borderColor = 'var(--error-50, #F4210B)';
+  }
+}
