e7452d9253b0adef90f6dd7356b9473d045fa1b5
Muslim Mamadaev <mamad.muslim@mail.ru>
2023-10-06 05:10:00 +0300

    add templates

--- public/components/Authorization/auth-page.js
+++ public/components/Authorization/auth-page.js
@@ -1,70 +1,37 @@
 import { renderRegPage } from '../Registration/reg-page.js';
+import { API } from '../../utils/api.js';
 import { createLabeledInput } from '../Input/input.js';
 import { renderFeedPage } from '../Feed/Feed.js';
 import { emailValid, passwordValid, nameValid } from '../../utils/valid.js';
-import { API } from '../../utils/api.js';
+import Handlebars from 'handlebars';
 
 /**
-* Рендерится страница аутентификации.
-* @param {HTMLElement} headerElement - Элемент заголовка.
-* @param {HTMLElement} pageElement - Элемент страницы.
-*/
-export function renderAuthPage(headerElement, pageElement) {
+ * Рендерится страница аутентификации.
+ * @param {HTMLElement} headerElement - Элемент заголовка.
+ * @param {HTMLElement} pageElement - Элемент страницы.
+ */
+export async function renderAuthPage(headerElement, pageElement) {
     const Api = new API();
 
     pageElement.innerHTML = '';
-    document.body.style.overflow = 'hidden';
 
-    const cancelButton = document.createElement('button');
-    cancelButton.classList.add('cancel-button');
-    const cancelIcon = document.createElement('img');
-    cancelIcon.src = './static/svg/cancel.svg';
-    cancelButton.appendChild(cancelIcon);
-    pageElement.appendChild(cancelButton);
+    const templateResponse = await fetch('../templates/Auth.hbs');
+    const templateText = await templateResponse.text();
+    const template = Handlebars.compile(templateText);
 
-    const ImageElement = document.createElement('img');
-    ImageElement.classList.add('ibackground');
-    ImageElement.src = 'static/img/image_2.png';
-    pageElement.appendChild(ImageElement);
+    const templateData = {
+        
+    };
 
-    const whiteBlock = document.createElement('div');
-    whiteBlock.classList.add('form-container');
-    pageElement.appendChild(whiteBlock);
+    const html = template(templateData);
 
-    const form = document.createElement('form');
-    form.classList.add('input-container');
-    whiteBlock.appendChild(form);
+    pageElement.innerHTML = html;
 
-    const usernameInput = createLabeledInput('Имя пользователя', 'text', 'username');
-    const passwordInput = createLabeledInput('Пароль', 'password', '●●●●●●●●●●●●');
-
-    const registrationTitle = document.createElement('h4');
-    registrationTitle.textContent = 'Войти в PinSpire';
-    registrationTitle.classList.add('auth-title');
-    form.appendChild(registrationTitle);
-
-    form.appendChild(usernameInput);
-    form.appendChild(passwordInput);
-
-    const AuthButton = document.createElement('button');
-    AuthButton.classList.add('button');
-    AuthButton.type = 'submit';
-    AuthButton.textContent = 'Войти';
-    form.appendChild(AuthButton);
-
-    const textContainer = document.createElement('p');
-    textContainer.classList.add('already-registered');
-
-    const alreadyHaveAccountText = document.createTextNode('Все еще нет аккаунта?');
-
-    const signUpLink = document.createElement('a');
-    signUpLink.href = '#';
-    signUpLink.textContent = 'Регистрация';
-
-    textContainer.appendChild(alreadyHaveAccountText);
-    textContainer.appendChild(signUpLink);
-
-    form.appendChild(textContainer);
+    const signUpLink = pageElement.querySelector('.already-registered a');
+    signUpLink.addEventListener('click', function (e) {
+        e.preventDefault();
+        renderRegPage(headerElement, pageElement);
+    });
 
     signUpLink.addEventListener('click', function (e) {
         e.preventDefault();
