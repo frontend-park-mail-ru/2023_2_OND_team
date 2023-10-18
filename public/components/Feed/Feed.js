8e3be70b0de819ac8462ced08c6c2c1b858d848b
Roman Sulaymanov <92111647+Romanlock@users.noreply.github.com>
2023-10-18 08:29:04 +0300

    add menu fo feed

--- public/components/Feed/Feed.js
+++ public/components/Feed/Feed.js
@@ -151,6 +151,22 @@ function definePageElements() {
     }
   }
 
+
+  var menuBtn = document.querySelector('.menu-btn');
+  var nav = document.querySelector('nav');
+  var lineOne = document.querySelector('nav .menu-btn .line--1');
+  var lineTwo = document.querySelector('nav .menu-btn .line--2');
+  var lineThree = document.querySelector('nav .menu-btn .line--3');
+  var link = document.querySelector('nav .nav-links');
+  menuBtn.addEventListener('click', () => {
+      nav.classList.toggle('nav-open');
+      lineOne.classList.toggle('line-cross');
+      lineTwo.classList.toggle('line-fade-out');
+      lineThree.classList.toggle('line-cross');
+      link.classList.toggle('fade-in');
+  })
+
+
   const scrollFunc = debounce(handleScroll, 100);
   window.scrollFunc = scrollFunc;
   scrollFunc();
