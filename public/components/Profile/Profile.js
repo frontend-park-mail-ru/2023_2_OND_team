fd10612b1d4895d566f5fe3b88b0e174454e042a
Romanlock <92111647+Romanlock@users.noreply.github.com>
2023-10-28 07:52:34 +0300

    add user data rendering

--- public/components/Profile/Profile.js
+++ public/components/Profile/Profile.js
@@ -5,51 +5,50 @@ import { renderFeedPage } from "../Feed/Feed.js";
 import { API } from "../../utils/api.js";
 
 export function renderProfilePage(headerElement, pageElement) {
-    const profile = Handlebars.templates['Profile.hbs'];
-    const context = {};
     
-    pageElement.innerHTML = profile(context);
+    API.getUserInfo() 
+        .then((data) => {
+            const profile = Handlebars.templates['Profile.hbs'];
+            const context = {};
+            pageElement.innerHTML = profile(context);
 
-    async function loadUserData() {
-        try {
-          return await API.getUserInfo();
-        } catch (error) {
-          console.error('Ошибка при получении данных о пользователе:', error);
-        }
-    }
-
-    const userInfo = loadUserData();
-
-    const logo = document.querySelector('.js-header__logo');
-    if (logo) {
-        logo.addEventListener('click', (e) => {
-            e.preventDefault();
-            renderFeedPage();
-        })
-    }
-
-    const userBtn = document.querySelector('.js-profile__menu__user-btn');
-    if (userBtn) {
-        userBtn.addEventListener('click', (e) => {
-            e.preventDefault();
+            const userInfo = data;
             renderUserPage(userInfo.username, userInfo.avatar);
-        });
-        renderUserPage(userInfo.username, userInfo.avatar);
-    }
 
-    const dataBtn = document.querySelector('.js-profile__menu__data-btn');
-    if (dataBtn) {
-        dataBtn.addEventListener('click', (e) => {
-            e.preventDefault();
-            renderDataPage(userInfo.username, userInfo.name, userInfo.surname, userInfo.avatar);
-        });
-    }
+            const logo = document.querySelector('.js-header__logo');
+            if (logo) {
+                logo.addEventListener('click', (e) => {
+                    e.preventDefault();
+                    renderFeedPage();
+                })
+            }
 
-    const securityBtn = document.querySelector('.js-profile__menu__security-btn');
-    if (securityBtn) {
-        securityBtn.addEventListener('click', (e) => {
-            e.preventDefault();
-            renderSecurityPage(userInfo.email);
+            const userBtn = document.querySelector('.js-profile__menu__user-btn');
+            if (userBtn) {
+                userBtn.addEventListener('click', (e) => {
+                    e.preventDefault();
+                    renderUserPage(userInfo.username, userInfo.avatar);
+                });
+            }
+
+            const dataBtn = document.querySelector('.js-profile__menu__data-btn');
+            if (dataBtn) {
+                dataBtn.addEventListener('click', (e) => {
+                    e.preventDefault();
+                    renderDataPage(userInfo.username, userInfo.name, userInfo.surname, userInfo.avatar);
+                });
+            }
+
+            const securityBtn = document.querySelector('.js-profile__menu__security-btn');
+            if (securityBtn) {
+                securityBtn.addEventListener('click', (e) => {
+                    e.preventDefault();
+                    renderSecurityPage(userInfo.email);
+                });
+            }
+        })
+        .catch((error) => {
+            console.error('Ошибка при получении данных о пользователе:', error);
         });
-    }
+
 }
