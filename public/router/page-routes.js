d15fbb055fdcba35d46e2b174c5a731ef745cc32
Muslim Mamadaev <mamad.muslim@mail.ru>
2023-11-01 21:40:59 +0300

    add webpack

---
+++ public/router/page-routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+const renderAuthPage = require('../components/Authorization/AuthPage.js');
+
+function handleRequest(req, res) {
+  const { method, url } = req;
+
+  if (method === 'GET' && url === '/auth') {
+    renderAuthPage(req, res);
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.end('<h1>Страница не найдена</h1>');
+  }
+}
+
+module.exports = handleRequest;
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path')
+const HTMLWebPackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    context: path.resolve(__dirname, 'public'),
+    mode: 'development',
+    entry: {
+        main: './index.js'
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HTMLWebPackPlugin({
+            template: './index.html'
+        })
+    ]
+}
