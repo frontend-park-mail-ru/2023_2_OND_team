7df8afaa75829dde8c0825f0f9e4e0da63ff94cb
Muslim Mamadaev <mamad.muslim@mail.ru>
2023-11-02 19:12:43 +0300

    update webpack config 3

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path')
-const HTMLWebPackPlugin = require('html-webpack-plugin')
-const {CleanWebPackPlugin} = require('clean-webpack-plugin')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
 module.exports = {
     context: path.resolve(__dirname, 'public'),
@@ -13,9 +13,9 @@ module.exports = {
         path: path.resolve(__dirname, 'dist')
     },
     plugins: [
-        new HTMLWebPackPlugin({
+        new HTMLWebpackPlugin({
             template: './index.html'
         }),
-        new CleanWebPackPlugin(),
+        new CleanWebpackPlugin(),
     ]
 }
