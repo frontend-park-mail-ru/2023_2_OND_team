58a0ba16bb82866fb8ccdd3a186203bbf63e383d
Romanlock <92111647+Romanlock@users.noreply.github.com>
2023-11-06 17:58:23 +0300

    rollback paths

--- server/index.js
+++ server/index.js
@@ -34,16 +34,9 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.get('*', (req, res) => {
-  const referer = req.headers.referer?.toString();
-  if (!referer) {
-    res.sendFile(path.resolve(__dirname, '..', 'src', 'index.html'));
-    return;
-  } else {
-    console.log(referer, (referer.split('/').length - 3))
-    res.sendFile(path.resolve(__dirname,  (referer.split('/').length - 3) * '..', 'src', 'index.html'));
-  }
+  res.sendFile(path.resolve(__dirname, '..', 'src', 'index.html'));
 });
 
 app.use(cors());
-const server = https.createServer(httpsOptions, app);
+// const server = https.createServer(httpsOptions, app);
 // server.listen(3000);
