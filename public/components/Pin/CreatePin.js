6443a11e9bf874065442354d378821b1c26740a9
Muslim Mamadaev <mamad.muslim@mail.ru>
2023-11-06 20:26:22 +0300

    update cancel-button3

--- public/components/Pin/CreatePin.js
+++ public/components/Pin/CreatePin.js
@@ -8,7 +8,7 @@ import {renderFeedPage} from '../Feed/Feed.js';
 export function renderCreatePin() {
     document.body.style.overflow = 'visible';
     const createPins = Handlebars.templates['CreatePins.hbs'];
-    const rootElement = rootElement.getElementById('root');
+    const rootElement = document.getElementById('root');
 
     const context = {};
     rootElement.innerHTML = createPins(context);
