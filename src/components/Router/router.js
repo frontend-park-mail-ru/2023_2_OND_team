1a4566b054ea0ee1c6f629205ccd83af34b6f4f1
Romanlock <92111647+Romanlock@users.noreply.github.com>
2023-11-07 09:13:57 +0300

    add log

--- src/components/Router/router.js
+++ src/components/Router/router.js
@@ -328,6 +328,7 @@ export class Router {
     handlePopstate() {
         const path = window.location.pathname;
         const route = this.#routes.find((r) => r.path === path);
+        console.log(route)
 
         switch (true) {
             case route:
