62fafd94ef3c69aa7259170da1d4ec35415d1865
Muslim Mamadaev <mamad.muslim@mail.ru>
2023-11-07 21:43:03 +0300

    update create pin4

--- src/utils/api.js
+++ src/utils/api.js
@@ -462,7 +462,7 @@ export class API {
         const response = await fetch(configItem.url, {
           method: 'POST',
           headers: {
-            'Content-Type': 'multipart/form-data',
+            'Content-Type': 'image/png',
             'x-csrf-token': this.state.getCsrfToken(),
           },
           body: JSON.stringify({picture, title, description, 'public': true}),
