8c8b11f1906cbd26f548409b50559864dbad40b0
Romanlock <92111647+Romanlock@users.noreply.github.com>
2023-11-07 13:09:38 +0300

    Update Feed.js

--- src/views/Feed/Feed.js
+++ src/views/Feed/Feed.js
@@ -74,7 +74,9 @@ export function renderFeedPage() {
                     renderPins(section, data.pins);
                     definePins();
     
-                    constblockonst pinsToDelete = Array.from(pins).slice(0, 20);
+                    const pins = document.querySelectorAll('.gallery__item');
+                    if (pins?.length > 100) {
+                        const pinsToDelete = Array.from(pins).slice(0, 20);
                         pinsToDelete.forEach(pin => {
                             pin.remove();
                         });
@@ -120,97 +122,3 @@ export function renderFeedPage() {
 
     
 }
-
-// /**
-// * Определяет обработчики событий для кнопок в шапке страницы и генерирует пины.
-// */
-// function definePageElements() {
-//     const headerElement = document.getElementById('header');
-//     const pageElement = document.getElementById('main');
-
-    
-
-//     const logoutBtn = document.querySelector('.js-header-btns__logout');
-//     if (logoutBtn) {
-//         logoutBtn.addEventListener('click', (e) => {
-//             e.preventDefault();
-//             headerElement.classList.add('header-hidden');
-//             pageElement.classList.add('main-no-padding');
-
-//             if (API.logoutUser()) {
-//                 headerElement.innerHTML = '';
-//                 window.removeEventListener('scroll', window.scrollFunc);
-//                 logo.removeEventListener('click', handleLogoClick);
-//                 // renderAuthPage(headerElement, pageElement);
-//             }
-//         });
-//     }
-
-//     const loginBtn = document.querySelector('.js-header-btns__login');
-//     if (loginBtn) {
-//         loginBtn.addEventListener('click', (e) => {
-//             e.preventDefault();
-//             headerElement.classList.add('header-hidden');
-//             pageElement.classList.add('main-no-padding');
-//             headerElement.innerHTML = '';
-//             window.removeEventListener('scroll', window.scrollFunc);
-//             logo.removeEventListener('click', handleLogoClick);
-//             // renderAuthPage(headerElement, pageElement);
-//         });
-//     }
-
-//     const signupBtn = document.querySelector('.js-header-btns__signup');
-//     if (signupBtn) {
-//         signupBtn.addEventListener('click', (e) => {
-//             e.preventDefault();
-//             headerElement.classList.add('header-hidden');
-//             pageElement.classList.add('main-no-padding');
-//             headerElement.innerHTML = '';
-//             window.removeEventListener('scroll', window.scrollFunc);
-//             logo.removeEventListener('click', handleLogoClick);
-//             // renderRegPage(headerElement, pageElement);
-//         });
-//     }
-
-//     const menuBtn = document.querySelector('.header__nav-create__menu');
-//     if (menuBtn) {
-//         const navCreate = document.querySelector('.nav-create');
-//         const navCreateLineOne = document.querySelector('.header__nav-create__menu-line1');
-//         const navCreateLineTwo = document.querySelector('.header__nav-create__menu-line2');
-//         const navCreateLineThree = document.querySelector('.header__nav-create__menu-line3');
-//         const links = document.querySelector('.header__nav-create__btns');
-//         menuBtn.addEventListener('click', () => {
-//             navCreate.classList.toggle('nav-open');
-//             navCreateLineOne.classList.toggle('line-cross');
-//             navCreateLineTwo.classList.toggle('line-fade-out');
-//             navCreateLineThree.classList.toggle('line-cross');
-//             links.classList.toggle('fade-in');
-//         });
-//     }
-
-//     const headerUserData = document.querySelector('.js-header-user__data');
-//     if (headerUserData) {
-//         const navUser = document.querySelector('.nav-user');
-//         const links = document.querySelector('.header__nav-user__btns');
-//         headerUserData.addEventListener('click', (e) => {
-//             if (!navUser.contains(e.target)) {
-//                 navUser.classList.toggle('nav-open');
-//                 links.classList.toggle('fade-in');
-//             }
-//         });
-//     }
-
-//     const profileBtn = document.querySelector('.js-header-btns__profile');
-//     if (profileBtn) {
-//         profileBtn.addEventListener('click', (e) => {
-//             e.preventDefault();
-//             window.removeEventListener('scroll', window.scrollFunc);
-//             logo.removeEventListener('click', handleLogoClick);
-//             // renderProfilePage(headerElement, pageElement);
-//         });
-//     }
-
-    
-
-//     // defineSidebar();
-// }
