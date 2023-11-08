457da3aa3229e95810689bff9df073aa527542ab
Romanlock <92111647+Romanlock@users.noreply.github.com>
2023-11-08 11:43:48 +0300

    updete profile security validation

--- src/views/ProfileSecurity/ProfileSecurity.js
+++ src/views/ProfileSecurity/ProfileSecurity.js
@@ -1,5 +1,6 @@
 import { API } from "../../utils/api.js";
 import { Router } from "../../components/Router/router.js";
+import { emailValid, passwordValid } from "../../components/Validation/valid.js";
 
 export function renderProfileSecurity() {
     const main = document.querySelector('#main');
@@ -16,7 +17,7 @@ export function renderProfileSecurity() {
             main.innerHTML = profileSecurityTemplate(profileSecurityContext);
 
             const footerChangeBtn = document.querySelector('.profile-security__change');
-            const footerSaveBtns = document.querySelector('.profile-security__save')
+            const footerSaveBtns = document.querySelector('.profile-security__save');
 
             const passwordField = document.querySelector('.profile-security__data-password');
             const passwordRepeatField = document.querySelector('.profile-security__data-password-repeat');
@@ -25,9 +26,14 @@ export function renderProfileSecurity() {
             const passwordInput = document.querySelector('.js-profile-security__data-password-data');
             const passwordRepeatInput = document.querySelector('.js-profile-security__data-password-repeat-data');
 
-            const mailTextarea = document.querySelector('.js-mail-textarea')
-            const passwordTextarea = document.querySelector('.js-password-textarea')
-            const passwordRepeatTextarea = document.querySelector('.js-password-repeat-textarea')
+            const mailTextarea = document.querySelector('.js-mail-textarea');
+            const passwordTextarea = document.querySelector('.js-password-textarea');
+            const passwordRepeatTextarea = document.querySelector('.js-password-repeat-textarea');
+
+            const securitySpan = document.querySelector('.profile-security-span');
+            const emailSpan = document.querySelector('.profile-security__email-span');
+            const passwordSpan = document.querySelector('.profile-security__password-span');
+            const passwordRepeatSpan = document.querySelector('.profile-security__password-repeat-span');
 
             const changeBtn = document.querySelector('.profile-security__btn-change');
             changeBtn?.addEventListener('click', () => {
@@ -68,39 +74,61 @@ export function renderProfileSecurity() {
                 passwordTextarea.value = '';
                 passwordRepeatTextarea.value = '';
 
-                const passwordSpan = document.querySelector('.profile-security__password-span');
-                const passwordRepeatSpan = document.querySelector('.profile-security__password-repeat-span');
-
+                securitySpan.textContent = '';
+                emailSpan.textContent = '';
+                passwordSpan.textContent = '';
                 passwordRepeatSpan.textContent = '';
             });
 
             const saveBtn = document.querySelector('.js-profile-security__btns__save-btn');
             saveBtn?.addEventListener('click', () => {
+                const email = mailInput.value;
                 const password = passwordTextarea.value;
                 const passwordRepeat = passwordRepeatTextarea.value;
-                if (password !== passwordRepeat) {
-                    const passwordSpan = document.querySelector('.profile-security__password-span');
-                    const passwordRepeatSpan = document.querySelector('.profile-security__password-repeat-span');
 
-                    passwordSpan.classList.add('span-error');
-                    passwordRepeatSpan.classList.add('span-error');
-
-                    passwordRepeatSpan.textContent = 'Пароли не совпадают';
-
-                    return;
+                const emailValidationResult = emailValid(email);
+                if (!emailValidationResult.valid) {
+                    // emailSpan.classList.add('span-error');
+                    emailSpan.textContent = emailValidationResult.message;
+                } else {
+                    emailSpan.textContent = '';
                 }
 
+                const passwordValidationResult = passwordValid(password);
+                if (!passwordValidationResult.valid) {
+                    passwordSpan.textContent = passwordValidationResult.message;
+                } else {
+                    passwordSpan.textContent = '';
+                }
+                const passwordRepeatValidationResult = passwordValid(passwordRepeat);
+                if (!passwordRepeatValidationResult.valid) {
+                    passwordRepeatSpan.textContent = passworpasswordRepeatValidationResultdValidationResult.message;
+                } else {
+                    passwordRepeatSpan.textContent = '';
+                }
+
+                if (password !== passwordRepeat) {
+                    passwordRepeatSpan.textContent = 'Пароли не совпадают';
+                } else {
+                    passwordRepeatSpan.textContent = '';
+                }
+
+                if ( !(emailValidationResult.valid && passwordValidationResult.valid &&
+                    passwordRepeatValidationResult.valid && (password === passwordRepeat)) ) {
+                        return;
+                    }
+
                 const data = {
-                    email: mailTextarea.value,
-                    password: passwordTextarea.value,
+                    email: email,
+                    password: password,
                 }
 
                 API.putUserInfo(data)
                     .then((status) => {
                         if (status === "ok") {
                             router.navigate('/profile/security');
-                        } else {
-                            console.error('error saving security');
+                        } else {                            
+                            securitySpan.textContent(`Некорректные данные: ${message}`);
                         }
                     })
                     .catch((error) => {
