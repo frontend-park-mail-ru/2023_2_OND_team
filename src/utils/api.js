26114443c45c07ce9e13777a95ca64c7ea8c01f9
Romanlock <92111647+Romanlock@users.noreply.github.com>
2023-11-08 09:42:58 +0300

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,23 @@
 import State from "../components/State/state.js";
 
+function getCookie(cookieName) {
+  const name = `${cookieName}=`;
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const cookieArray = decodedCookie.split(';');
+
+  for (let i = 0; i < cookieArray.length; i++) {
+    let cookie = cookieArray[i];
+    while (cookie.charAt(0) === ' ') {
+      cookie = cookie.substring(1);
+    }
+    if (cookie.indexOf(name) === 0) {
+      return cookie.substring(name.length, cookie.length);
+    }
+  }
+
+  return '';
+}
+
 export class API {
     static state = new State();
 
@@ -68,6 +86,8 @@ export class API {
         if (!configItem) {
           throw new Error('Не найдена конфигурация для checkLogin');
         }
+        const myCookieValue = getCookie('_csrf');
+        console.log('csrf', myCookieValue);
 
         let response;
 
