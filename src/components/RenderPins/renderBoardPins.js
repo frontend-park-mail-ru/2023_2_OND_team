10b0705cc9aed77a0f1dc5ba0f8bb218005d30c4
Muslim Mamadaev <mamad.muslim@mail.ru>
2023-11-10 01:45:51 +0300

    update add pins in board18

--- src/components/RenderPins/renderBoardPins.js
+++
@@ -1,8 +0,0 @@
-export function renderPins(parent, images) {
-    const template = Handlebars.templates['PinsOnBoard.hbs'];
-    images.forEach((image) => {
-      const context = {src: image.picture, id: image.id};
-  
-      parent?.insertAdjacentHTML('beforeend', template(context));
-    });
-}
