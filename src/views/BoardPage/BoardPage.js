631d6fcd07458ba8919dbfaa872381e33b2c9478
Muslim Mamadaev <mamad.muslim@mail.ru>
2023-11-11 23:24:05 +0300

    update board page7

--- src/views/BoardPage/BoardPage.js
+++ src/views/BoardPage/BoardPage.js
@@ -3,43 +3,42 @@ import State from "../../components/State/state.js";
 import { Router } from "../../components/Router/router.js";
 import { renderPins } from "../../components/RenderPins/renderPins.js";
 
-export function renderBoardPage(boardID) {
+export async function renderBoardPage(boardID) {
     const router = new Router();
     const main = document.querySelector('#main');
     const state = new State();
     const boardPage = Handlebars.templates['BoardPage.hbs'];
 
-    API.getBoardInfo(boardID)
-        .then((boardInfo) => {
-            console.log('Информация о доске:', boardInfo);
+    try {
+        const boardInfo = await API.getBoardInfo(boardID);
+        console.log('Информация о доске:', boardInfo);
 
-            const context = {
-                title: boardInfo.title,
-                description: boardInfo.description
-            };
+        const context = {
+            title: boardInfo.title,
+            description: boardInfo.description
+        };
 
-            main.innerHTML = boardPage(context);
+        main.innerHTML = boardPage(context);
 
-            const usernameReal = state.getUsername();
+        const usernameReal = state.getUsername();
 
-            const deleteButton = document.createElement('button');
-            deleteButton.textContent = 'Удалить';
-            deleteButton.classList.add('delete-button');
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Удалить';
+        deleteButton.classList.add('delete-button');
 
-            const updateButton = document.createElement('img');
-            updateButton.src = 'https://pinspire.online:1445/assets/icons/actions/icon_edit.svg';
-            updateButton.classList.add('edit-button');
+        const updateButton = document.createElement('img');
+        updateButton.src = 'https://pinspire.online:1445/assets/icons/actions/icon_edit.svg';
+        updateButton.classList.add('edit-button');
 
-            console.log(usernameReal);
+        console.log(usernameReal);
 
-            const rec = document.querySelector('.bar');
-            rec.appendChild(deleteButton);
-            rec.appendChild(updateButton);
+        const rec = document.querySelector('.bar');
+        rec.appendChild(deleteButton);
+        rec.appendChild(updateButton);
 
-            renderPins(boardInfo.pins);
-        })
-        .catch((error) => {
-            console.error('Ошибка при получении информации о доске:', error);
-            router.navigate('/page404');
-        });
+        renderPins(document.getElementById('board-pins'), boardInfo.pins);
+    } catch (error) {
+        console.error('Ошибка при получении информации о доске:', error);
+        router.navigate('/page404');
+    }
 }
