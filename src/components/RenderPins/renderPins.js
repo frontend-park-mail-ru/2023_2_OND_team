69f4436686c5f3797bb7b7322d71d50c40f947b4
Muslim Mamadaev <mamad.muslim@mail.ru>
2023-11-12 02:29:49 +0300

    update add pins14

--- src/components/RenderPins/renderPins.js
+++ src/components/RenderPins/renderPins.js
@@ -4,7 +4,7 @@
 * @param {Array} images - Массив объектов с информацией о пинах.
 */
 export function renderPins(parent, images) {
-  const template = Handlebars.templates['Pin.hbs'];
+  const template = Handlebars.templates['Pins.hbs'];
   images.forEach((image) => {
     const context = {src: image.picture, id: image.id};
 
