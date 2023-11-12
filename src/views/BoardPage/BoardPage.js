b014108e824c78e8c308241d9df121755e47bbe4
Muslim Mamadaev <mamad.muslim@mail.ru>
2023-11-12 18:38:43 +0300

    update board page2

--- src/views/BoardPage/BoardPage.js
+++ src/views/BoardPage/BoardPage.js
@@ -1,8 +1,3 @@
-import { API } from "../../utils/api.js";
-import State from "../../components/State/state.js";
-import { Router } from "../../components/Router/router.js";
-import { renderPins } from "../../components/RenderPins/renderPins.js";
-
 export async function renderBoardPage(boardID) {
     const router = new Router();
     const main = document.querySelector('#main');
@@ -42,15 +37,15 @@ export async function renderBoardPage(boardID) {
         rec.appendChild(deleteButton);
         rec.appendChild(updateButton);
 
-        renderBoardPins();
+        await renderBoardPins();
     } catch (error) {
         console.error('Ошибка при получении информации о доске:', error);
         router.navigate('/page404');
     }
 
-    function renderBoardPins() {
+    async function renderBoardPins() {
         try {
-            const data = API.getBoardPins(boardID);
+            const data = await API.getBoardPins(boardID);
             const section = document.getElementById('board-pins');
             renderPins(section, data.pins);
         } catch (error) {
