bc894fe6ab57206b820070888b8a47b7aac73db2
Muslim Mamadaev <mamad.muslim@mail.ru>
2023-11-12 18:39:33 +0300

    update board page3

--- src/views/BoardPage/BoardPage.js
+++ src/views/BoardPage/BoardPage.js
@@ -1,3 +1,8 @@
+import { API } from "../../utils/api.js";
+import State from "../../components/State/state.js";
+import { Router } from "../../components/Router/router.js";
+import { renderPins } from "../../components/RenderPins/renderPins.js";
+
 export async function renderBoardPage(boardID) {
     const router = new Router();
     const main = document.querySelector('#main');
