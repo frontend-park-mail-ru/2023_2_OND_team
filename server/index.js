9c3c40129edbba95a06ccf6e8face1c14c03a68f
Romanlock <92111647+Romanlock@users.noreply.github.com>
2023-11-13 16:42:50 +0300

    change styles

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ const httpsOptions = {
   cert: fs.readFileSync('/home/ond_team/cert/fullchain.pem'),
 };
 
-https.createServer(httpsOptions, app).listen(443);
+https.createServer(httpsOptions, app).listen(1444);
 
 app.use(express.static(path.resolve(__dirname, '..', 'src')));
 app.use(express.json());
