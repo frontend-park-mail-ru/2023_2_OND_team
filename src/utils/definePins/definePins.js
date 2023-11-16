d20c9d51b698efadc4dd5caefb285a72e7812860
Romanlock <92111647+Romanlock@users.noreply.github.com>
2023-11-16 14:40:19 +0300

    Update definePins.js

--- src/utils/definePins/definePins.js
+++ src/utils/definePins/definePins.js
@@ -85,11 +85,11 @@ export function definePins() {
                             .then((data) => {
                                 state.setLike(pinEssence.ID, false);
                                 const countLikes = state.getCountLikes(pinEssence.ID);
-                                // if (countLikes === null) {
-                                    state.setCountLikes(pinEssence.ID, data.count_like);
+                                if (countLikes === null) {
                                     likeField.innerHTML = state.getCountLikes(pinEssence.ID);
                                     likeField.style.opacity = 1;
-                                // }
+                                }
+                                state.setCountLikes(pinEssence.ID, data.count_like);
                             })
                             .catch((error) => {
                                 console.error(error);
@@ -108,11 +108,11 @@ export function definePins() {
                             .then((data) => {
                                 state.setLike(pinEssence.ID, true);
                                 const countLikes = state.getCountLikes(pinEssence.ID);
-                                // if (countLikes === null) {
-                                    state.setCountLikes(pinEssence.ID, data.count_like);
+                                if (countLikes === null) {
                                     likeField.innerHTML = state.getCountLikes(pinEssence.ID);
                                     likeField.style.opacity = 1;
-                                // }
+                                }
+                                state.setCountLikes(pinEssence.ID, data.count_like);
                             })
                             .catch((error) => {
                                 console.error(error);
