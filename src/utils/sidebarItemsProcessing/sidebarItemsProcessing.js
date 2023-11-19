0c18f7e748b347a3de5014f83110fb8fb9171fda
Romanlock <92111647+Romanlock@users.noreply.github.com>
2023-11-19 09:19:10 +0300

    add sidebar image processing

--- src/utils/sidebarItemsProcessing/sidebarItemsProcessing.js
+++ src/utils/sidebarItemsProcessing/sidebarItemsProcessing.js
@@ -1,10 +1,19 @@
 export function setActiveSidebarItem(item) {
-    const activeElement = document.querySelector('.sidebar__menu__btn-active');
-    activeElement?.classList.remove('sidebar__menu__btn-active');
-
     if (!item) {
         return;
     }
+
+    const activeElement = document.querySelector('.sidebar__menu__btn-active');
+    activeElement?.classList.remove('sidebar__menu__btn-active');
+
+    const activeElementName = activeElement.className.split(' ')[1].split('__')[2];
+    const activeIcon = document.querySelector(`.sidebar__menu__${activeElementName}__icon-img`);
+    activeIcon.src = activeIcon.getAttribute('src').substring(0, activeIcon.getAttribute('src').length - 11) + 
+                     activeIcon.getAttribute('src').substring(activeIcon.getAttribute('src').length - 4);
+
+    const nestedIcon = document.querySelector(`.sidebar__menu__${item}__icon-img`);
+    nestedIcon.src = nestedIcon.getAttribute('src').substring(0, nestedIcon.getAttribute('src').length - 4) + 
+                     '-active' + nestedIcon.getAttribute('src').substring(nestedIcon.getAttribute('src').length - 4);
 
     const nestedBtn = document.querySelector(`.sidebar__menu__${item}`);
     nestedBtn?.classList.add('sidebar__menu__btn-active');
@@ -16,4 +25,5 @@ export function setActiveSidebarItem(item) {
         profileFields.classList.remove('hide');
         profileArrow.src = '/assets/icons/actions/icon_profile_arrow-up.svg';
     }
+    
 }
