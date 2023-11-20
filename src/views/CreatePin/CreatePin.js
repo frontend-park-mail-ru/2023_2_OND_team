ba0755c1ee98aad202c19a90efe9580403704ccd
Muslim Mamadaev <mamad.muslim@mail.ru>
2023-11-21 01:36:22 +0300

    update create pin

--- src/views/CreatePin/CreatePin.js
+++ src/views/CreatePin/CreatePin.js
@@ -1,7 +1,7 @@
 import { API } from "../../utils/api.js";
 import State from "../../components/State/state.js";
 import { Router } from "../../components/Router/router.js";
-
+ 
 export function renderCreatePin() {
     const router = new Router();
     const main = document.querySelector('#main');
@@ -9,30 +9,30 @@ export function renderCreatePin() {
     const createPin = Handlebars.templates['CreatePin.hbs'];
     const context = {};
     let picture;
-
+ 
     main.innerHTML = createPin(context);
-
+ 
     const cancelButton = document.querySelector('.js-pin-cancel__btn-change');
     const createButton = document.querySelector('.js-pin-create__btn-change');
     const pictureInput = document.getElementById('picture');
     const uploadImage = document.querySelector('.upload-image');
-
+ 
     cancelButton.addEventListener('click', function (e) {
         e.preventDefault();
         router.navigate('/');
     });
-
+ 
     createButton.addEventListener('click', function (e) {
         e.preventDefault();
         const title = document.getElementById('title').value;
         const description = document.getElementById('description').value;
-    
+ 
         const formData = new FormData();
         formData.append('picture', picture);
         formData.append('title', title);
         formData.append('description', description);
         formData.append('public', true);
-    
+ 
         API.createPin(formData)
             .then((response) => {
                 if (response.status === 'ok') {
@@ -45,21 +45,21 @@ export function renderCreatePin() {
                 console.error(error);
             });
     });    
-
+ 
     pictureInput.addEventListener('change', (event) => {
         const pictureFile = event.target.files[0];
         handleFile(pictureFile);
     });
-
+ 
     uploadImage.addEventListener('dragenter', preventDefaults);
     uploadImage.addEventListener('dragover', preventDefaults);
     uploadImage.addEventListener('drop', handleDrop);
-
+ 
     function preventDefaults(event) {
         event.stopPropagation();
         event.preventDefault();
     }
-
+ 
     function handleDrop(event) {
         if (event.target === uploadImage) {
             preventDefaults(event);
@@ -70,20 +70,29 @@ export function renderCreatePin() {
             }
         }
     }
-
+ 
     function handleFile(file) {
         const reader = new FileReader();
-
-        reader.onload = (event) => {
+        const pictureImg = document.querySelector('.upload-image');
+ 
+        reader.onload = (event) => {            
+        	const arrayBuffer = event.target.result;
+        	const byteArray = new Uint8Array(arrayBuffer);
+        	const byteString = String.fromCharCode.apply(null, byteArray);
+        	const base64String = btoa(byteString);
+        	const dataUrl = 'data:' + file.type + ';base64,' + base64String;
+ 
+        	pictureImg.src = dataUrl;
+ 
             const pictureBytes = event.target.result;
             const mimeType = file.type;
             picture = new Blob([pictureBytes], { type: mimeType });
-
+ 
             console.log(picture);
-
+ 
             //pictureInput.value = file.name;
         };
-
+ 
         reader.readAsArrayBuffer(file);
     }
 }
