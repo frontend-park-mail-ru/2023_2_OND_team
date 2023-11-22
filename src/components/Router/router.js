bded5d5bfa91302948ddfb9e0ca74e6a56d4b11f
Roman Sulaymanov <92111647+Romanlock@users.noreply.github.com>
2023-11-22 14:12:19 +0300

    Update router.js

--- src/components/Router/router.js
+++ src/components/Router/router.js
@@ -427,6 +427,8 @@ export class Router {
                         return;
                     }
 
+                    console.log(this.state.getUserID, userID)
+
                     if (this.state.getUserID === userID) {
                         this.navigate('/profile');
                         return;
