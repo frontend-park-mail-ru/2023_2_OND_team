0069bd8361ecbc17d539fec01f7cfdb2ad835ba8
Muslim Mamadaev <mamad.muslim@mail.ru>
2023-11-24 19:41:15 +0300

    update header

--- src/views/HeaderDefault/HeaderDefault.js
+++ src/views/HeaderDefault/HeaderDefault.js
@@ -81,6 +81,12 @@ export function renderHeaderDefault() {
         userMenu.classList.toggle('hide');
     });
 
+    const filterBtn = document.querySelector('.header__filter__img-image');
+    const filterList = document.querySelector('.header__filter__list');
+    filterBtn?.addEventListener('click', () => {
+        filterList.classList.toggle('hide');
+    });
+
     document.body.addEventListener('click', (e) => {
         if (e.target !== document.querySelector('.js-create-img')) {
             createMenu.classList.add('hide');
@@ -90,11 +96,8 @@ export function renderHeaderDefault() {
             userMenu.classList.add('hide');
             profileArrow.src = '/assets/icons/actions/icon_profile_arrow-down.svg';
         }
+        if (e.target !== document.querySelector('.header__filter__img-image')) {
+            filterList.classList.add('hide');
+        }
     })
-
-    const filterBtn = document.querySelector('.header__filter__img-image');
-    const filterList = document.querySelector('.header__filter__list');
-    filterBtn?.addEventListener('click', () => {
-        filterList.classList.toggle('hide');
-    });
 }
