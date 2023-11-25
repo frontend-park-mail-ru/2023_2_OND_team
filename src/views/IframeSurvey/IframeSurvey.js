7415640a974c44c48627f1f71509bb0536ac3548
Roman Sulaymanov <92111647+Romanlock@users.noreply.github.com>
2023-11-25 15:31:16 +0300

    update iframe

---
+++ src/views/IframeSurvey/IframeSurvey.js
@@ -0,0 +1,18 @@
+export function renderIframeSurvey(surveyID) {
+  if (surveyID == -1) {
+    const rootElement = document.querySelector('#root');
+
+    const iframe = document.createElement('iframe');
+    iframe.src = `https://pinspire.online:1446/${surveyID}`;
+    iframe.classList.add('hide');
+    iframe.classList.add('iframe-survey');
+
+    rootElement.appendChild(iframe);
+
+    return;
+  }
+
+  const iframe = document.querySelector('.iframe-survey');
+  iframe?.src = `https://pinspire.online:1446/${surveyID}`;
+  iframe?.classList.remove('hide');
+}
