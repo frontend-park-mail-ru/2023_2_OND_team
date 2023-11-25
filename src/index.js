4ab3638615804c7c50c83b35376da501f5385dfb
Muslim Mamadaev <mamad.muslim@mail.ru>
2023-11-25 14:45:46 +0300

    add rec quiz10

--- src/index.js
+++ src/index.js
@@ -5,15 +5,20 @@ document.addEventListener('DOMContentLoaded', () => {
         button.addEventListener('click', () => {
             const index = button.dataset.index;
 
+            let activeCount = 0;
+
             for (let i = 1; i <= index; i++) {
                 const img = document.querySelector(`.js-like-btn[data-index="${i}"]`);
                 img.src = '/assets/icons/actions/like_active.svg';
+                activeCount++;
             }
 
             for (let i = parseInt(index) + 1; i <= 5; i++) {
                 const img = document.querySelector(`.js-like-btn[data-index="${i}"]`);
                 img.src = '/assets/icons/actions/like.svg';
             }
+
+            console.log('Количество активных сердечек:', activeCount);
         });
     });
 });
