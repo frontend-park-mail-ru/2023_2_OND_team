7d94ac05cb2d03cdc1d849d105fe94cf8bb325e6
Muslim Mamadaev <mamad.muslim@mail.ru>
2023-11-25 17:44:22 +0300

    update40

--- src/views/Quiz_Rec/quiz_rec.js
+++ src/views/Quiz_Rec/quiz_rec.js
@@ -92,7 +92,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     quizID = 2;
                     inputValue = null;
                     console.log(quizID, [activeCount, inputValue]);
-                    //window.parent.postMessage(message, '*')
+                    window.parent.postMessage([quizID, activeCount, inputValue], 'https://pinspire.online/')
                 });
             }  
         });
