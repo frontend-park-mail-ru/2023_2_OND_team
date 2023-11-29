3c1b10bc989168d5ddf387c92cc3138ac5ce1eb9
Muslim Mamadaev <mamad.muslim@mail.ru>
2023-11-29 03:52:10 +0300

    test update2

--- src/views/PinPage/PinPage.js
+++ src/views/PinPage/PinPage.js
@@ -12,8 +12,7 @@ export function renderPinPage(pinID) {
 
     API.getPinInfo(pinID)
         .then((pinInfo) => {
-            console.log('Информация о пине:', pinInfo);
-            console.log(pinInfo.author.id);
+            console.log('Информация о пине:', pinInfo.author.id);
             const context = {
                 id: pinInfo.id,
                 src: pinInfo.picture,
