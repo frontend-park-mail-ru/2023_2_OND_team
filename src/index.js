1bd2fd4a6ca506bf7996ae9bbc7ec867a39b82ff
Romanlock <92111647+Romanlock@users.noreply.github.com>
2023-12-17 14:33:29 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,9 +10,11 @@ import { API } from './utils/Api/api.js';
 import { Router } from "./components/Router/router.js";
 import State from './components/State/state.js';
 import { WebSocketConnection } from './utils/Api/messenger/messengerWS.js';
+import { Notifications } from './views/Notifications/Notifications.js';
 
 const router = new Router();
 const state = new State();
+const notifications = new Notifications();
 
 API.getCsrfToken()
     .then(() => {
@@ -20,5 +22,15 @@ API.getCsrfToken()
           .then(() => {
             router.handlePopstate();
             const WS = new WebSocketConnection(`wss://${state.getDomain()}:8080/websocket/connect/chat?${state.getUserID()}`);
+
+            WS.setOnMessageMethod((event) => {
+              const jsonObject = JSON.parse(event.data);
+              if (jsonObject.message.eventType === 'create') {
+                  notifications.renderNotification('NEW_MESSAGE', jsonObject?.message?.message?.from)
+              } else {
+                  console.log(jsonObject);
+              }
           });
+          });
+          
     });
