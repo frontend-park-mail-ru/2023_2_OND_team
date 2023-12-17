a35babde0433fb948a96b255aa7503ad4b193a3f
Romanlock <92111647+Romanlock@users.noreply.github.com>
2023-12-17 15:19:35 +0300

    Update messengerWS.js

--- src/utils/Api/messenger/messengerWS.js
+++ src/utils/Api/messenger/messengerWS.js
@@ -11,27 +11,27 @@ export class WebSocketConnection {
         this.url = url;
         this.state = new State();
         this.socket = new WebSocket(url);
-        this.socket.onopen = this.onOpen.bind(this);
+        // this.socket.onopen = this.onOpen.bind(this);
         this.socket.onmessage = this.onMessage.bind(this);
         this.socket.onerror = this.onError.bind(this);
         this.socket.onclose = this.onClose.bind(this);
 
     }
 
-    onOpen(event) {
-        console.log('WebSocket connection opened:', event);
+    // onOpen(event) {
+    //     console.log('WebSocket connection opened:', event);
 
-        const wsConnectMessage = {
-            "requestID": 0,
-            "action": "Subscribe",
-            "channel":{
-                "name": String(this.state.getUserID()),
-                "topic": "chat"
-            }
-        }
+    //     const wsConnectMessage = {
+    //         "requestID": 0,
+    //         "action": "Subscribe",
+    //         "channel":{
+    //             "name": String(this.state.getUserID()),
+    //             "topic": "chat"
+    //         }
+    //     }
         
-        this.sendMessage(JSON.stringify(wsConnectMessage));
-    }
+    //     this.sendMessage(JSON.stringify(wsConnectMessage));
+    // }
 
     onMessage(event) {
         console.log('WebSocket message received:', event.data);
