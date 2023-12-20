6d420ec98140feb771b88255b3bc2d8f6797f06b
Romanlock <92111647+Romanlock@users.noreply.github.com>
2023-12-20 07:13:51 +0300

    add no comments message

--- src/views/PinPage/Comments/Comments.js
+++ src/views/PinPage/Comments/Comments.js
@@ -6,7 +6,20 @@ export class Comments {
     }
 
     renderAllComments(comments) {
-        comments?.forEach((comment) => this.renderComment(comment));
+        if (comments) {
+            comments.forEach((comment) => this.renderComment(comment));
+        } else {
+            const noCommentsDiv = document.createElement('div');
+            noCommentsDiv.classList.add('pin-comments__no_content_div');
+
+            const noCommentsMessage = document.createElement('span');
+            noCommentsMessage.classList.add('pin-comments__no_content_message');
+            noCommentsMessage.content = 'Оставьте первый комментарий';
+
+            noCommentsDiv.appendChild(noCommentsMessage);
+
+            this.#commentsDiv.appendChild(noCommentsDiv);
+        }
     }
 
     renderComment(comment) {
