ba0dacba9b211c88ba3869edf312e4dbcb605fff
Romanlock <92111647+Romanlock@users.noreply.github.com>
2023-12-26 20:34:58 +0300

    update styles

--- src/utils/HeaderTitleProcessing/headerTitleProcessing.js
+++ src/utils/HeaderTitleProcessing/headerTitleProcessing.js
@@ -1,5 +1,5 @@
 export function setHeaderTitle(title) {
-  if (window.innerWidth < 900) return;    
+  if (window.innerWidth < 700) return;    
 
   const headerSearch = document.querySelector('.header__search');
   const headerLeft = document.querySelector('.header__left');
@@ -14,7 +14,7 @@ export function setHeaderTitle(title) {
 }
 
 export function removeHeaderTitle() {
-  if (window.innerWidth < 900) return;    
+  if (window.innerWidth < 700) return;    
   
   const headerSearch = document.querySelector('.header__search');
   const headerLeft = document.querySelector('.header__left');
