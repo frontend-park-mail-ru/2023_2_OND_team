ac5356a9ac91f7e440fe4893c8ce603574d57524
Romanlock <92111647+Romanlock@users.noreply.github.com>
2023-12-27 21:15:10 +0300

    Update ProfileSecurity.js

--- src/views/ProfileSecurity/ProfileSecurity.js
+++ src/views/ProfileSecurity/ProfileSecurity.js
@@ -56,6 +56,7 @@ export function renderProfileSecurity() {
 
         const canselBtn = document.querySelector('.js-profile-security__btns__cansel-btn');
         canselBtn?.addEventListener('click', () => {
+          securitySpan.classList.add('hide');
           footerChangeBtn.classList.remove('hide');
           footerSaveBtns.classList.add('hide');
 
